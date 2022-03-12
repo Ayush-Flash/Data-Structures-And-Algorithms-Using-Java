94aa864f1ffe70918ac88f36ad6f02e185266190
Ayush Mishra <ayushmishra@Ayushs-MacBook-Pro.local>
2022-03-12 19:40:15 +0530

    Added Power Computing Recursive Approach

---
+++ Mathematics/Power/Main.java
@@ -0,0 +1,26 @@
+package Mathematics.Power;
+
+import java.util.*;
+
+public class Main {
+
+    public static int ans = -1;
+
+    public static int calculate(int x, int y) {
+        if(y == 1) {
+            return x;
+        }
+        int halfExpo = calculate(x, y / 2);
+        int ans = halfExpo * halfExpo;
+        if(y % 2 != 0) {
+            ans *= x;  
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(calculate(x, y));
+    }
+}
