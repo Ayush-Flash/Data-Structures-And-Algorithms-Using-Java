1560e8b2fda0b166ebb6dbf35e33060615edb5cb
Ayush Mishra <ayushmishra@Ayushs-MacBook-Pro.local>
2022-03-13 08:43:50 +0530

    Added Sum Of Natural Numbers Using Recursive Approach

---
+++ Recursion/NaturalNumberSum/Main.java
@@ -0,0 +1,26 @@
+package Recursion.NaturalNumberSum;
+
+import java.util.*;
+
+public class Main {
+    
+    public static int sum = 0;
+    public static int sumOfNNumbers(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if(n == 1) {
+            return 1;
+        }
+
+        sum = n + sumOfNNumbers(n - 1);
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sumOfNNumbers(n);
+        System.out.println(sum);
+    }
+}
