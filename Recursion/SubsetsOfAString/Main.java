c365bcb6540c9fc67300656eebf60f8c661c35a9
Ayush Mishra <ayushmishra@Ayushs-MacBook-Pro.local>
2022-03-18 14:24:41 +0530

    Added Intutive Approach To Solve Generate Subset Solution

--- Recursion/SubsetsOfAString/Main.java
+++ Recursion/SubsetsOfAString/Main.java
@@ -17,9 +17,35 @@ public class Main {
         return subsets;
     }
 
+    public static int subsetIndex = 0;
+
+    public String[] generateSubsetsIntutive(String str, int currentIndex, String[] subsets) {
+        if(currentIndex < 0) {
+            subsets[subsetIndex++] = "";
+            return subsets;
+        }
+
+        String[] smallerSubsets = generateSubsetsIntutive(str, currentIndex - 1, subsets);
+
+        int currentSmallOutputSize = subsetIndex;
+        for(int i = currentSmallOutputSize ; i > 0 ; i--) {
+            subsets[subsetIndex++] = smallerSubsets[currentSmallOutputSize - i] + str.charAt(currentIndex);
+        }
+        return subsets;
+    }
+
     public List<String> generateSubsetOfString(String str) {
         List<String> subsets = new ArrayList<>();
         return generateSubsets(str, "", 0, subsets);
+    }
+
+    public void fun(String str) {
+        int currentIndex = str.length() - 1;
+        String[] subsets = new String[(int)Math.pow(2, currentIndex + 1)];
+        subsets =  generateSubsetsIntutive(str, currentIndex, subsets);
+        for(String s : subsets) {
+            System.out.println(s);
+        }
     }
 
     public static void main(String[] args) {
@@ -27,6 +53,7 @@ public class Main {
             String str = sc.nextLine();
             Main mObj = new Main();
             System.out.println(mObj.generateSubsetOfString(str));
+            //mObj.fun(str);
         }
     }
 }
