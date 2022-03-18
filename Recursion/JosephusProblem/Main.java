63c37ff734c8a1817d9904a39de93996b8c1949a
Ayush Mishra <ayushmishra@Ayushs-MacBook-Pro.local>
2022-03-18 19:00:40 +0530

    Added Jospehus Problem Recursive Code

---
+++ Recursion/JosephusProblem/Main.java
@@ -0,0 +1,23 @@
+package Recursion.JosephusProblem;
+
+import java.util.*;
+
+public class Main {
+    
+    public int getSurvivor(int numberOfPeople, int passCount) {
+        if(numberOfPeople == 1) {
+            return 1;
+        }
+        int smallOutputSurvivor = getSurvivor(numberOfPeople - 1, passCount);
+        return (smallOutputSurvivor + passCount) % numberOfPeople;
+    }
+    
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+            int numberOfPeople = sc.nextInt();
+            int passCount = sc.nextInt();
+            Main mObj = new Main();
+            System.out.println(mObj.getSurvivor(numberOfPeople, passCount));
+        }
+    }
+}
